90bc0b74dfc8bbb91eb0a05903c7006e75872628
ggf <georgeg.fedoroff@gmail.com>
2010-03-02 11:26:31 +0300

    correct edit conductor

--- zapuski/cp/conductors.php
+++ zapuski/cp/conductors.php
@@ -19,8 +19,8 @@ elseif (isset($show) || isset($edit)|| isset($add) )
 	$r = getright($user);
 
 		echo "<form method=post id=editform>";
-		if (isset($edit)) {
-			$sql="SELECT *, customers.id AS cusid, plates.id AS plid FROM conductors JOIN (customers,plates) ON (conductors.board_id=plates.id AND plates.customer_id=customers.id) WHERE conductors.id='$edit'";
+		if ($edit!=0) {
+			$sql="SELECT *, customers.id AS cusid, conductors.board_id AS plid FROM conductors JOIN (customers,plates) ON (conductors.board_id=plates.id AND plates.customer_id=customers.id) WHERE conductors.id='$edit'";
 			$res = mysql_query($sql);
 			if ($rs=mysql_fetch_array($res)) {
 				$customer=$rs["cusid"];
@@ -28,36 +28,26 @@ elseif (isset($show) || isset($edit)|| isset($add) )
 				$side = $rs["side"];
 				$pib=$rs["pib"];
 				$lays=$rs["lays"];
-				echo "<input type=hidden name=edit value=$edit>";
+				echo "Заказчик:<input type=input readonly style='background-color:gray;' value='".$rs["customer"]."'><br>";
+				echo "Плата:<input type=input readonly style='background-color:gray;' value='".$rs["plate"]."'>";
+				echo "<input type=hidden name=plate_id value='$plid'>";
 			}
 		} else {
-			$disabled = "disabled";
-		}
-		echo "Заказчик:<SELECT name=customer_id ".($edit!=0?"disabled":"")." id=cusid onchange=\"var plat=$.ajax({url:'http://".$_SERVER['HTTP_HOST']."/zapuski/zd.php',data:'cusid='+$('#cusid').val()+'&selectplates',async:false}).responseText; $('#plates').html(plat); $('#addplbut').removeAttr('disabled');$('#plates').removeAttr('disabled');\">";
-		$sql = "SELECT * FROM customers ORDER BY customer";
-		$res = mysql_query($sql);
-		while ($rs=mysql_fetch_array($res)) {
-			echo "<option value=".$rs["id"]." ".($rs["id"]==$customer?"SELECTED":"").">".$rs["customer"];
-		}
-		echo "</SELECT><!--input type=button value='Добавить' onclick='window.location=\"http://".$_SERVER['HTTP_HOST'].$GLOBALS["PHP_SELF"]."?zd&addcus\";'--><br>";
-		echo "Плата:<SELECT name=plate_id disabled id=plates>";
-		if(isset($edit)) {
-			$sql = "SELECT * FROM plates WHERE customer_id='$customer' ORDER BY plate ";
-			//echo $sql;
+			echo "Заказчик:<SELECT name=customer_id ".($edit!=0?"readonly":"")." id=cusid onchange=\"var plat=$.ajax({url:'http://".$_SERVER['HTTP_HOST']."/zapuski/zd.php',data:'cusid='+$('#cusid').val()+'&selectplates',async:false}).responseText; $('#plates').html(plat); $('#plates').removeAttr('disabled');\">";
+			$sql = "SELECT * FROM customers ORDER BY customer";
 			$res = mysql_query($sql);
 			while ($rs=mysql_fetch_array($res)) {
-				echo "<option value=".$rs["id"]." ".($rs["id"]==$plid?"SELECTED":"").">".$rs["plate"];
+				echo "<option value=".$rs["id"].">".$rs["customer"];
 			}
+			echo "</SELECT><br>";
+			echo "Плата:<SELECT name=plate_id disabled id=plates>";
+			echo "</SELECT>";
 		}
-		echo "</SELECT><!--input id=addplbut type=button value=Добавить $disabled onclick='window.location=\"http://".$_SERVER['HTTP_HOST'].$GLOBALS["PHP_SELF"]."?zd&addpl&cusid=\"+$(\"#cusid\").val();'-->";
 		echo "<br>Плат в блоке:<input size=3 name=pib value=$pib>";
 		echo "<br>Пластин:<input size=3 name=lays value=$lays>";
-		$ldate = substr($ldate,8,2).".".substr($ldate,5,2).".".substr($ldate,0,4);
 		echo "<br>Сторона:<select name=side><option value='TOP'>TOP<option value='BOT'>BOT<option value='TOPBOT'>TOPBOT";
-		//echo "<br>Дата запуска:<input size=10 name=ldate id=datepicker Value=$ldate><br>";
-		//echo "<script>$('#datepicker').datepicker($.datepicker.regional['ru']);</script>";
 		echo "<input type=hidden name=accept value='yes'>";
-		echo "<input type='hidden' value='".(isset($edit)?$edit:"0")."' name='edit'>";
+		echo "<input type=hidden value='$edit' name='edit'>";
 		echo "<input type=hidden name=tid value='$tid'>";
 		echo "<input type=hidden name=uid value='$uid'>";
 
@@ -68,15 +58,13 @@ elseif (isset($show) || isset($edit)|| isset($add) )
 	else 
 		{
 		// сохранение
-		$ldate=substr($ldate,6,4)."-".substr($ldate,3,2)."-".substr($ldate,0,2);
 		
 		if ($edit!=0) {
-			$sql = "UPDATE conductors SET board_id='$plate_id', pib='$pib', side='$side', lays='$lays' WHERE id='$edit'";
+			$sql = "UPDATE conductors SET board_id='$plate_id', pib='$pib', side='$side', lays='$lays', user_id='$userid', ts=NOW() WHERE id='$edit'";
 			
 		} else {
-			$sql = "INSERT INTO conductors (board_id,pib,side,lays) VALUES('$plate_id','$pib','$side','$lays')";
+			$sql = "INSERT INTO conductors (board_id,pib,side,lays,user_id,ts) VALUES('$plate_id','$pib','$side','$lays','$userid',NOW())";
 		}
-		mylog1($sql);
 		if (!mysql_query($sql)) {
 			my_error("Не удалось изменить Кондуктор");
 		} else {
